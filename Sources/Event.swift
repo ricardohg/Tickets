c41a24e359f5fb4ced4c366ea90e783875a47c3a
Ricardo Hernandez <a.ricardohg@gmail.com>
2018-08-07 15:30:14 -0500

    added event object with postgres setup

--- Sources/Event.swift
+++ Sources/Event.swift
@@ -5,10 +5,40 @@ class Event: PostgresStORM {
     var id: Int = 0
     var name: String = ""
     var description: String = ""
-    var price: Double = 0
+    var price: Float = 0
+    var imageurl: String = ""
     
     override func table() -> String {
         return "events"
     }
     
+    override func to(_ this: StORMRow) {
+        id = this.data["id"] as? Int ?? 0
+        name = this.data["name"] as? String ?? ""
+        description = this.data["description"] as? String ?? ""
+        price = this.data["price"] as? Float ?? 0
+        imageurl = this.data["imageurl"] as? String ?? ""
+    }
+    
+    func rows() -> [Event] {
+        return self.results.rows.map { acronym in
+            
+            let row = Event()
+            
+            row.to(acronym)
+            
+            return row
+            
+        }
+    }
+    
+    func asDictionary() -> [String: Any] {
+        return [
+            "id": id,
+            "name": name,
+            "description": description,
+            "price": price,
+            "imageurl": imageurl
+        ]
+    }
 }
--- Sources/main.swift
+++ Sources/main.swift
@@ -2,10 +2,44 @@ import PerfectLib
 import PerfectHTTPServer
 import PerfectHTTP
 
+import StORM
+import PostgresStORM
+
+PostgresConnector.host = "localhost"
+PostgresConnector.username = "tickets"
+PostgresConnector.password = "password"
+PostgresConnector.database = "ticketsdb"
+PostgresConnector.port = 5432
+
+let setupObj = Event()
+try? setupObj.setup()
+
 let server = HTTPServer()
 server.serverPort = 8080
 
+func events(request: HTTPRequest, response: HTTPResponse) {
+    do {
+        let getObj = Event()
+        try getObj.findAll()
+        
+        var events: [[String: Any]] = []
+        
+        for row in getObj.rows() {
+            events.append(row.asDictionary())
+        }
+        
+        try response.setBody(json: events)
+            .setHeader(.contentType, value: "Application/json")
+            .completed()
+        
+    } catch  {
+        response.setBody(string: "error handling the request")
+            .completed(status: .internalServerError)
+    }
+}
+
 var routes = Routes()
+routes.add(method: .get, uri: "/events", handler: events)
 server.addRoutes(routes)
 
 do {
